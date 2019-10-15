1403c608017ffd9cd3f0076b2794176609143bcc
MondaleFelix <mondalefelix@yahoo.com>
2019-10-15 14:33:35 -0700

    extended UIView for autolayout constraints

--- TinderBee/TinderBee/ViewController.swift
+++ TinderBee/TinderBee/ViewController.swift
@@ -13,8 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+        
+        let redView = UIView()
+        redView.backgroundColor = .red
+        
+        let blueView = UIView()
+        blueView.backgroundColor = .blue
+        
+        let stackView = UIStackView(arrangedSubviews: [redView, blueView])
+        stackView.distribution = .fillEqually
+        stackView.axis = .vertical
+        
+        view.addSubview(stackView)
+        
+        stackView.frame = .init(x: 0, y: 0, width: 300, height: 200)
+        
+//       
+        stackView.fillSuperview()
 
+        
+    }
 
 }
 
